2e800ec04ea170f9e843a552c986890b9c1c589f
KrishDhungana <kdhungana@hawk.iit.edu>
2023-03-02 13:27:13 -0600

    Adding in JS
    Adding in click me code for a discount on the first year of the newsletter.

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// A function to display a message
+function sayCode() {
+    alert("USE DISCOUNT CODE: FULLCOURT");
+}
+
+// Call function on click of the button
+document.getElementById("myBtn").onclick = sayCode;
