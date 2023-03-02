705f9ed2fc0afd9057224b6fd860ea5a133f2336
KrishDhungana <kdhungana@hawk.iit.edu>
2023-03-02 13:43:58 -0600

    Fixing Indentation
    Fixing indentation in all files

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // A function to display a message
 function sayCode() {
-    alert("USE DISCOUNT CODE: FULLCOURT");
+   alert("USE DISCOUNT CODE: FULLCOURT");
 }
 
 // Call function on click of the button
